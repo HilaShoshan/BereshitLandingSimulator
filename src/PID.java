54263f0dc1fb8cd122ce39bb1f1e6e518ceb5399
Hila Shoshan <hilashoshan0605@gmail.com>
2022-05-14 23:59:38 +0300

    pid class done

--- src/PID.java
+++ src/PID.java
@@ -1,29 +1,4 @@
-//public class PID {
-//
-//    private double tp, td, ti;  // the variables for the PID Controller
-//    private double true_value = 1;  // the desired altitude
-//    private double sum_cte = 0;
-//    private double derivative_cte, last_cte;
-//    
-//
-//    public PID(double tp, double td, double ti) {
-//        this.tp = tp;
-//        this.td = td;
-//        this.ti = ti;
-//    }
-//
-//    public double compute(double current_value) {
-//        double cte = current_value - this.true_value;
-//        this.derivative_cte = last_cte - cte;
-//        this.sum_cte += cte;  // integral
-//        double alpha = -tp * cte -td * derivative_cte - ti * sum_cte;
-//        this.last_cte = cte;
-//        return alpha;
-//    }
-//}
-
-
-
+// taken from the open source: https://github.com/tekdemo/MiniPID-Java
 
 /**
 * Small, easy to use PID implementation with advanced controller capability.<br> 
